18a2cf08eb0e0875f66a502bf5c1453f0d763610
Lucas de Oliveira <lucasdeoliveira@Lucass-MBP-2.lan>
2022-03-14 11:07:17 -0700

    fixed bug

--- wordle.py
+++ wordle.py
@@ -17,11 +17,11 @@ def get_vocab(dictionary_file="/usr/share/dict/words"):
 def remove_words(vocab, input_word, output):
     '''
     Inputs:
-        * set
+        * vocab - set of all allowable words
         * input_word - word sent to worddle (str)
         * output - list of numbers indicating matches
-            (-1 = no match, 0 = wrong position, 1 = right position)
-            (-1 = grey, 0 = yellow, 1 = green)
+            (0 = no match, 1 = wrong position, 2 = right position)
+            (0 = grey, 1 = yellow, 2 = green)
     
     Function does not return anything, just removes invalid words
     from the vocab set.
@@ -29,11 +29,19 @@ def remove_words(vocab, input_word, output):
     remove_words = set()
     for i in range(5):
         letter, match = input_word[i], output[i]
-        if match == -1:
+        # Remove words containing invalid letter
+        if match == 0:
             [remove_words.add(word) for word in vocab if letter in word]
-        elif match == 0:
+        # Remove words with valid letter in wrong position
+        # and remove words without valid letter
+        elif match == 1:
             [remove_words.add(word) for word in vocab if letter == word[i]]
+            [remove_words.add(word) for word in vocab if letter not in word]
+        # Remove words without valid letter in right position
         else:
             [remove_words.add(word) for word in vocab if letter != word[i]]
     for word in remove_words:
         vocab.remove(word)
+        
+        
+
